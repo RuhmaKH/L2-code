ccae584df14b4124ac31418887b8a75f40334687
Mathuran <mathuran752000@gmail.com>
2019-12-14 13:39:39 +0100

    arbre

--- Java/Project/Arbre.java
+++ Java/Project/Arbre.java
@@ -1,15 +1,36 @@
 import javax.swing.*;
 import java.awt.*;
+import java.io.*;
+import javax.imageio.ImageIO;
 
 public class Arbre extends Obstacle{
+  private Image image= null;
+
 
   public Arbre (){
     super("Arbre");
+    try {
+      image= ImageIO.read(new File("./Image/arbreblanc_retouche.png"));
+    }
+    catch(IOException exc) {
+      exc.printStackTrace();
+    }
   }
 
   public Arbre( int x , int y ){
-    super( "Arbre",x ,y);
+    super( "Arbre" , x ,y);
+    try {
+      image= ImageIO.read(new File("./Image/arbreblanc_retouche.png"));
+    }
+    catch(IOException exc) {
+      exc.printStackTrace();
+    }
   }
 
-    public void dessiner (Graphics g){}
+    public void dessiner (Graphics g){
+      int tc = Monde.tailleCase;
+      g.drawImage(image,getX()*tc+1, getY()*tc+1, tc-2, tc-2, Monde.world);
+    }
+
+
 }
