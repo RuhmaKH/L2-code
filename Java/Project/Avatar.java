befe6580a1efa9737edd114ad4ebae9a97bcf205
UgoMou <55445670+UgoMou@users.noreply.github.com>
2019-12-14 13:52:08 +0100

    x

--- Java/Project/Avatar.java
+++ Java/Project/Avatar.java
@@ -206,7 +206,8 @@ public class Avatar extends Personnage{
   }
 
   public void update(){
-
+    int[] coord = MyKeyListener.getDirection();
+    seDeplacer(coord[0], coord[1]);
   }
   
   public double acheter (Acc acc){
