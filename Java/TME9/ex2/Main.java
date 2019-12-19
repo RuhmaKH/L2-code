2d39bc9a178c732b66c6dd50593d4b875bb9dc5c
Mathuran <mathuran752000@gmail.com>
2019-12-19 11:02:12 +0100

    image

--- Java/TME9/ex2/Main.java
+++ Java/TME9/ex2/Main.java
@@ -1,17 +1,17 @@
 public class Main{
 
     public static void main(String[] args){
-        
+
         MonTableau tab = new MonTableau(5);
         try{
             for (int i =0 ; i<10; i++){
                 tab.ajouter(i);
             }
         }catch(TabPleinException e){
-            System.out.println(e.getMessage());
-            //System.out.println(e);
+            //System.out.println(e.getMessage());
+            System.out.println(e);
             //e.printStackTrace(System.out);
         }
-        
+
     }
 }
